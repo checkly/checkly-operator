0ffdc1a8c2a216a8c85aa84ca67cd62373cd2dbd
Akos Veres <akosveres@users.noreply.github.com>
2022-07-28 09:07:12 +0300

    fix(rbac): Fix rbac permissions (#25)
    Some permissions were not generated correctly and other permissions were
    missing an apiGroup definition.
    
    Tested locally.

--- controllers/checkly/alertchannel_controller.go
+++ controllers/checkly/alertchannel_controller.go
@@ -40,10 +40,10 @@ type AlertChannelReconciler struct {
 	ApiClient checkly.Client
 }
 
-//+kubebuilder:rbac:groups=checkly.checklyhq.com,resources=alertchannels,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=checkly.checklyhq.com,resources=alertchannels/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=checkly.checklyhq.com,resources=alertchannels/finalizers,verbs=update
-//+kubebuilder:rbac:groups=,resources=secrets,verbs=get;list
+//+kubebuilder:rbac:groups=k8s.checklyhq.com,resources=alertchannels,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=k8s.checklyhq.com,resources=alertchannels/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=k8s.checklyhq.com,resources=alertchannels/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
